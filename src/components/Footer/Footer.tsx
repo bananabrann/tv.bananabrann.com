4def00d34d909f9c18fa02f52a6c2f3104ae5aaa
bananabrann <plbrannan95@gmail.com>
2023-02-17 17:55:54 -0600

    Footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,13 +8,17 @@ interface Props {
 const Footer: FC<Props> = ({ siteVersion }) => {
   return (
     <div className={footerStyles.container}>
-      <small>
-        <a href="https://github.com/bananabrann/tv.bananabrann.dev">
-          View source code
-        </a>
-      </small>
-      <small>|</small>
-      <small>Version {siteVersion}</small>
+      <div className={footerStyles.content}>
+        <small>
+          <a href="https://github.com/bananabrann/tv.bananabrann.dev">
+            View source code
+          </a>
+        </small>
+        <small>|</small>
+        <small>Version {siteVersion}</small>
+      </div>
+
+      <div className={footerStyles.rainbow} />
     </div>
   );
 };
