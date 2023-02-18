2ddea95e86127686cc799ec5dce3ca428932747b
bananabrann <plbrannan95@gmail.com>
2023-02-17 18:07:54 -0600

    Add logo and base styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Footer from "./components/Footer/Footer";
+import Logo from "./components/Logo/Logo";
+import styles from "./App.module.css";
+import "./App.css";
 
 const SITE_VERSION = "1.0.0";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <h1>Hello</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={styles.header}>
+        <Logo />
       </header>
 
       <Footer siteVersion={SITE_VERSION} />
